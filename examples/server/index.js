aa34326c547828a5f708bb36cd4b59c8034d6cfe
Mohamed Meabed <mo.meabed@gmail.com>
2022-01-19 23:31:06 -0500

    chore: update example

--- examples/server/index.js
+++ examples/server/index.js
@@ -3,6 +3,7 @@ const { CurrencyApi, Configuration } = require('@devmehq/sdk-js');
 const config = new Configuration({ apiKey: 'demo-key' });
 const apiInstance = new CurrencyApi(config);
 
+// promises without async/await
 apiInstance
   .v1ConvertCurrency({ amount: 10, from: 'USD', to: 'EUR' })
   .then((result) => {
@@ -10,4 +11,18 @@ apiInstance
   })
   .catch((error) => {
     console.error('error', error instanceof Error ? error.message : error);
+  });
+
+apiInstance
+  .v1ListCurrencies({ code: ['USD'] })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch(async (error) => {
+    console.error('error', error.status);
+    console.error('error', error.statusText);
+    return error.json();
+  })
+  .then((error) => {
+    console.error('error message from body', error?.message);
   });
