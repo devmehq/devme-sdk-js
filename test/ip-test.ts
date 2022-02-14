61c97d5fa7d0363f6794b730c15f422de2e6c1f5
Mohamed Meabed <mo.meabed@gmail.com>
2022-02-13 21:37:27 -0500

    chore: update

--- test/ip-test.ts
+++ test/ip-test.ts
@@ -4,15 +4,16 @@ import { expect } from 'chai';
 describe('ip', async function () {
   it('should get the correct data', async function () {
     const { data } = await ipApi.v1GetIpDetails({ ip: '52.45.23.11' });
-    expect(data).to.be.eql({
+    expect(data).to.be.contain({
       asn: 14618,
       aso: 'AMAZON-AES',
       city: {
+        areaCode: '0',
         accuracyRadius: 1000,
         latitude: 39.0469,
         longitude: -77.4903,
-        metroCode: 511,
         name: 'Ashburn',
+        region: 'Virginia',
         timeZone: 'America/New_York',
       },
       countryCode: 'US',
